aa45b61675bb1d0eb3f185512886d8b62e9f36c7
Maxperathy <maxwellapeadido1@gmail.com>
2023-02-17 12:25:58 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry point
+ * Description: print all numbers in base 10 starting from 0 to 9
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+	{
+
+		printf("%d", a);
+
+	}
+	putchar('\n');
+	return (0);
+
+}
