b3ba67a13066043eb492326652c4b5710e7c8244
Maxperathy <maxwellapeadido1@gmail.com>
2023-02-17 11:25:31 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description: prints the last digit of random number, showing >5, <6 or =0
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n, l;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	l = n % 10;
+	if (l > 5) 
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
+	}
+	else if (l == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, l);
+	}
+	else if (l < 6 && l != 0)
+	{
+		printf("Last digit of %d id %d and is less than 6 and not 0\n", n, l);
+	}
+	return (0);
+}
