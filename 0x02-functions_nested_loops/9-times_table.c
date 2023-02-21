ca038946496c23ad490d961f36a54860671e8139
Maxperathy <maxwellapeadido1@gmail.com>
2023-02-21 13:24:24 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ * Description: prints the 9 times table
+ * Return: void
+ */
+void times_table(void)
+{
+	int row, column, product;
+
+	for (row = 0; row < 10; row++)
+	{
+		product = row * column;
+		if (column == 0)
+		{
+			_putchar('0');
+		}
+		else if (product < 10)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(product + '0');
+		}
+		else
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar((product / 10) + '0');
+			_putchar((product % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
