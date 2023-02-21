5ee0be8fb1e47eeac26887a1863f7359a911be65
Maxperathy <maxwellapeadido1@gmail.com>
2023-02-21 06:01:25 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,15 @@
  */
 int main(void)
 {
-	char str[] = "_putchar\n";
+	char str[] = "_putchar";
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
+	
+	_putchar('\n');
 
 	return (0);
 }
