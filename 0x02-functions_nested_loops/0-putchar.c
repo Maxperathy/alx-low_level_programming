d53f646f1bc7b05f04f1c555cec589959e5c109d
Maxperathy <maxwellapeadido1@gmail.com>
2023-02-21 05:41:22 -0800

    a program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry Point
+ * Description: prints _putchar using putchar prototype
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char str[] = "_putchar\n";
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	return (0);
+}
