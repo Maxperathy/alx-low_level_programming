d27929734ab3892ff31642093c9653e908060359
Maxperathy <maxwellapeadido1@gmail.com>
2023-02-27 11:57:00 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: the string to print in reverse
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	while (len > 0)
+	{
+		_putchar(*(s - 1));
+		s--;
+		len--;
+	}
+
+	_putchar('\n');
+
